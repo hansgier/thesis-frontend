8128f7b4bbcaaf7879e48a3748dcbf3f6516cd8b
Hans Gier <aurorasupreme9001@gmail.com>
2024-07-23 14:17:15 +0800

    -finished

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,19 @@
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Form } from "antd";
+
 export const SearchBar = () => {
+    const location = useLocation();
+    const [form] = Form.useForm();
+    const { announcements } = useSelector((store) => store.announcements);
+    const { users4admin } = useSelector((store) => store.users);
+    const { projects } = useSelector((store) => store.projects);
+    const { contacts } = useSelector((store) => store.contacts);
 
     return (
-        <form
-            className="bg-white border border-white hover:border hover:border-[#96d7cf] hover:duration-200 hover:transition-all px-2 rounded-lg w-40 sm:w-2/4 md:w-2/3">
+        <Form form={ form }
+              className="bg-white border border-white hover:border hover:border-[#96d7cf] hover:duration-200 hover:transition-all px-2 rounded-lg w-40 sm:w-2/4 md:w-2/3"
+        >
             <div className="flex h-full items-center md:space-x-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                      stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
@@ -14,6 +25,6 @@ export const SearchBar = () => {
                     className="bg-transparent border-none flex focus:border-gray-800 focus:outline-none focus:ring-0 focus:ring-offset-0 font-normal h-full hover:cursor-text placeholder-opacity-100 placeholder-gray-400 px-1 md:px-3 py-2 text-sm tracking-wide w-full placeholder:text-xs md:placeholder:text-sm"
                     placeholder="Search" type="search" />
             </div>
-        </form>
+        </Form>
     );
 };
